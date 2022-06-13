f75298a734b4a270400014eff041fb6b6f84bc8c
playtunes <marcelmashaba@gmail.com>
2022-06-14 01:29:13 +0200

    Add project files.

---
+++ Encrypt/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Encrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fileName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(25, 335);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 0;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(84, 124);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(114, 20);
+            this.keyTextBox.TabIndex = 3;
+            this.keyTextBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password:";
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(290, 335);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.fileName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(25, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 30);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // fileName
+            // 
+            this.fileName.AutoSize = true;
+            this.fileName.Location = new System.Drawing.Point(38, 11);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(0, 13);
+            this.fileName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "File:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(287, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "AES ENCRYPTION";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Select a file";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 163);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(340, 147);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 431);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.encryptButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encrypt App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label fileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Encrypt/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Encrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        OpenFileDialog fileSelector = new OpenFileDialog();
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            //Checks if any file has been chosen.
+            if (!String.IsNullOrEmpty(fileSelector.FileName))
+            {
+                //Checks if a password has been entered. 
+                if (!String.IsNullOrWhiteSpace(keyTextBox.Text))
+                {
+                    string key = keyTextBox.Text;
+                    byte[] rawData = File.ReadAllBytes(fileSelector.FileName);
+                    listBox1.Items.Add("Encrypting...");
+                    using (AesCryptoServiceProvider aesObj = new AesCryptoServiceProvider())
+                    {
+                        aesObj.KeySize = 128;
+                        aesObj.BlockSize = 128;
+                        aesObj.Key = hashinAlg(key);
+                        aesObj.IV = hashinAlg(key);
+                        aesObj.Mode = CipherMode.CBC;
+                        aesObj.Padding = PaddingMode.PKCS7;
+
+
+                        byte[] encrypted = EncryptStringToBytes_Aes(rawData, aesObj.Key, aesObj.IV);
+                        File.WriteAllBytes(fileSelector.FileName, encrypted);
+                        listBox1.Items.Add("Encryption Successful!");
+
+                        keyTextBox.Clear();
+                    }
+                }
+                else
+                    MessageBox.Show("Enter a Password!");
+
+            }
+            else
+                MessageBox.Show("Select a valid File!");
+        }
+
+        static byte[] hashinAlg(String key)
+        {
+            byte[] bytekey;
+            byte[] hashedkey;
+            bytekey = Encoding.UTF8.GetBytes(key);
+
+            hashedkey = new MD5CryptoServiceProvider().ComputeHash(bytekey);
+            
+
+            return hashedkey;
+        }
+        [STAThread]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        static byte[] EncryptStringToBytes_Aes(byte[] byteData, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (byteData == null || byteData.Length <= 0)
+                throw new ArgumentNullException("byteData");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] encrypted;
+
+            // Creates an AesCryptoServiceProvider object with the specified key and IV
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Creates an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Creates the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(byteData, 0, byteData.Length);
+                        csEncrypt.FlushFinalBlock();
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Returns the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static byte[] DecryptStringFromBytes_Aes(byte[] encryptedData, byte[] Key, byte[] IV)
+        {
+            // Checks arguments.
+            if (encryptedData == null || encryptedData.Length <= 0)
+                throw new ArgumentNullException("encryptedData");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] decrypted;
+
+            // Creates an AesCryptoServiceProvider object with the specified key and IV
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+
+                // Creates a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Creates the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedData))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                    {
+
+                        csDecrypt.Write(encryptedData, 0, encryptedData.Length);
+                        csDecrypt.FlushFinalBlock();
+                        decrypted = msDecrypt.ToArray();
+                    }
+                }
+            }
+
+            return decrypted;
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            //checks if any file has been chosen
+            if (!String.IsNullOrEmpty(fileSelector.FileName))
+            {
+                //Checks if a password has been entered. 
+                if (!String.IsNullOrWhiteSpace(keyTextBox.Text))
+                {
+                    string key = keyTextBox.Text;
+                    byte[] rawData = File.ReadAllBytes(fileSelector.FileName);
+                    listBox1.Items.Add("Decrypting...");
+                    using (AesCryptoServiceProvider aesObj = new AesCryptoServiceProvider())
+                    {
+
+                        aesObj.KeySize = 128;
+                        aesObj.BlockSize = 128;
+                        aesObj.Key = hashinAlg(key);
+                        aesObj.IV = hashinAlg(key);
+                        aesObj.Mode = CipherMode.CBC;
+                        aesObj.Padding = PaddingMode.PKCS7;
+
+                        byte[] decrypted = DecryptStringFromBytes_Aes(rawData, aesObj.Key, aesObj.IV);
+                        File.WriteAllBytes(fileSelector.FileName, decrypted);
+                        listBox1.Items.Add("Decryption Successful!");
+
+                        keyTextBox.Clear();
+                    }
+                }
+                else
+                    MessageBox.Show("Enter a Password!");
+            }
+            else
+                MessageBox.Show("Select a valid File!");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           //Opens a dialog to choose a file.
+            fileSelector.ShowDialog();
+            fileName.Text = "..." + fileSelector.FileName.Substring(fileSelector.FileName.Length - 24);
+            listBox1.Items.Add("File: " + fileSelector.FileName+" selected!");
+
+        }
+    }
+}
